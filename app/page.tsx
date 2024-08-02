3f30e0777da2878b2132e0b97284f35dea5300de
Afnan Siddiqui <afnan.sid0@gmail.com>
2024-08-02 22:18:02 +0500

    deploying

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="relative min-h-screen overflow-hidden">
       <LampDemo className="absolute inset-0 -z-10" />
       <div className="relative z-10 text-white min-h-screen p-6 flex flex-col items-center">
-        <div className="max-w-4xl w-full mt-20">
+        <div className="max-w-4xl w-full mt-20"> 
           <PhotoUpload />
           <PhotoList />
         </div>
